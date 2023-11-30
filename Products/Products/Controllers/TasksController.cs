34873005625518ad31e9b28787c4dc62c1e83217
Viktor Luchyk <luchykvictor@gmail.com>
2023-11-30 17:07:19 +0200

    #7 some code refac

--- Products/Products/Controllers/TasksController.cs
+++ Products/Products/Controllers/TasksController.cs
@@ -6,22 +6,12 @@ namespace Sprint_13.Controllers
 {
     public class TasksController : Controller
     {
-        private Dictionary<string, int> ListProducts => new Dictionary<string, int>
-        {
-            {"Milk", 2 },
-            {"Bread", 2 },
-            {"Cake", 1 },
-            {"Ice Cream", 5 },
-            {"Cola", 10 },
-        };
+
         public IActionResult Greetings() => View();
 
         public IActionResult SprintTasks() => View();
 
-        public IActionResult ShoppingList()
-        {
-            return View(ListProducts);
-        }
+        public IActionResult ShoppingList() => View();
 
     }
 }
