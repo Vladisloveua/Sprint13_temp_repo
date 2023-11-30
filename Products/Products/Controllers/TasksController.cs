5875c8a36b65eb663d738843abf278c040dce3c1
Vlad_is_love <plombeeeer@gmail.com>
2023-11-30 10:14:59 +0200

    1-3 tasks added

---
+++ Products/Products/Controllers/TasksController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sprint_13.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Greetings() => View();
+
+         public IActionResult SprintTasks() => View();
+
+    }
+}
